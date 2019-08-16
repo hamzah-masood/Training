feaefadf25c150fc71fa9faa918ad11bc4c86963
Hamzah <mcs@MCSs-MacBook-Pro.local>
2019-08-16 13:38:16 -0400

    firstTest

---
+++ firstTest/firstTest/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  firstTest
+//
+//  Created by MCS on 8/16/19.
+//  Copyright © 2019 MCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var episodes: UITableView!
+  
+  var gameOfThronesArray: [Episode] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+    episodes.dataSource = self
+    episodes.delegate = self
+    episodes.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    
+    URLSession.shared.dataTask(with: URL(string: "https://api.tvmaze.com/shows/82?embed=seasons&embed=episodes")!) { (data, response, _) in
+      guard let data = data else { return }
+      let gameOfThrones = try? JSONDecoder().decode(GameOfThrones.self, from: data)
+      self.gameOfThronesArray = (gameOfThrones?.embedded.episodes)!
+      
+      DispatchQueue.main.async {
+        self.episodes.reloadData()
+      }
+      }.resume()
+    
+  }
+}
+
+struct GameOfThrones: Codable{
+  let embedded: Embedded
+  
+  enum CodingKeys: String, CodingKey{
+    case embedded = "_embedded"
+  }
+}
+
+struct Embedded: Codable{
+  let episodes: [Episode]
+}
+
+struct Episode: Codable{
+  let season: Int
+  let episodeNumber: Int
+  let episodeName: String
+  let premierDate: String
+  let airTime: String
+  let summary: String
+  
+  enum CodingKeys: String, CodingKey {
+    case season
+    case episodeNumber = "number"
+    case episodeName = "name"
+    case premierDate = "airdate"
+    case airTime = "airtime"
+    case summary
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return gameOfThronesArray.count //count of episodes
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+    cell.textLabel?.numberOfLines = 0
+    cell.textLabel?.text = "Season: " + "\(gameOfThronesArray[indexPath.row].season)" + " / Episode Number: " + "\(gameOfThronesArray[indexPath.row].episodeNumber)" + " /Episode Name: " + gameOfThronesArray[indexPath.row].episodeName
+    
+    return cell
+  }
+  
+}
+
+extension ViewController: UITableViewDelegate{
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //main storyboard
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //assigning next screen
+    let nextViewController = storyboard.instantiateViewController(withIdentifier: "nextViewController") as! nextViewController
+    
+    
+    nextViewController.updatedSeason = gameOfThronesArray[indexPath.row].season
+    nextViewController.updatedEpisodeNumber = gameOfThronesArray[indexPath.row].episodeNumber
+    nextViewController.updatedEpisodeName = gameOfThronesArray[indexPath.row].episodeName
+    nextViewController.updatedPremierDate = gameOfThronesArray[indexPath.row].premierDate
+    nextViewController.updatedAirTime = gameOfThronesArray[indexPath.row].airTime
+    nextViewController.updatedSummary = gameOfThronesArray[indexPath.row].summary
+    
+    
+    if let index = self.episodes.indexPathForSelectedRow {
+      self.episodes.deselectRow(at: index, animated: true)
+    }
+    
+    navigationController?.pushViewController(nextViewController, animated: true)
+    
+  }
+}
+
+
